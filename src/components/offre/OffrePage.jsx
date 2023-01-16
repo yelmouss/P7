fc72e4e85b2c4f8c03456146992f2b5f88598c66
yelmouss <yassine.elmouss@sfr.fr>
2023-01-16 19:19:29 +0100

    Offre page + Offer rating + page

--- src/components/offre/OffrePage.jsx
+++ src/components/offre/OffrePage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 import offres from '../../datas/logements.json';
 import Collapse from '../Template/Collapse';
-import Gallery from '../Template/Gallery';
+// import Gallery from '../Template/Gallery';
 import RatingScale from './Rating';
 
 const offrePage = () => {
@@ -19,9 +19,9 @@ const offrePage = () => {
 
     return (
         <section key={offre.id} className='offre_page'>
-            <Gallery 
+            {/* <Gallery 
                 img={offre.pictures} 
-            />
+            /> */}
 
             <header className='offre_page_header'>
                 <article className='offre_page_header_infos'>
---
+++ src/components/pages/offre.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "../Template/Header";
+import offrePage from "../offre/OffrePage";
+import Footer from "../Template/Footer"
+
+const Offre = () => {
+    return(
+        <>
+            <Header />
+            <offrePage />
+            <Footer />
+        </>
+    )
+}
+
+export default Offre;
