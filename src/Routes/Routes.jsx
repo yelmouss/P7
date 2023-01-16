a95132b000aaa077bdd17e070e2023820bd053bd
yelmouss <yassine.elmouss@sfr.fr>
2023-01-16 12:32:16 +0100

    add ABout componnent Page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from '../components/pages/Home';
+import About from '../components/pages/About';
 
 
 const Routing = () => {
@@ -10,6 +11,7 @@ const Routing = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route exact path="/home" element={<Home />} />     
+                <Route exact path="/About" element={<About />} />     
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// import AboutBanner from "../components/About/AboutBanner"
+// import AboutContent from "../components/About/AboutContent"
+import Header from "../Template/Header"
+import Footer from "../Template/Footer"
+
+const About = () => {
+    return(
+        <>
+            <Header />
+            {/* <AboutBanner />
+            <AboutContent /> */}
+            <Footer />
+        </>
+    )
+}
+
+export default About
