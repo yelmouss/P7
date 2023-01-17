6b2e47a1d6c54f0a672c23e0b4c91c97801290cb
yelmouss <yassine.elmouss@sfr.fr>
2023-01-17 16:07:42 +0100

    404 route and page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../components/pages/Home';
 import About from '../components/pages/About';
 import Offre from '../components/pages/offre';
+import ErrorNotFound from '../components/pages/404';
 
 
 const Routing = () => {
@@ -14,6 +15,10 @@ const Routing = () => {
                 <Route exact path="/home" element={<Home />} />     
                 <Route exact path="/About" element={<About />} />     
                 <Route exact path="/offre/:id" element={<Offre />} />
+                <Route exact path="/About" element={<About />} />     
+                <Route path="*" element={<ErrorNotFound />} />
+
+
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/pages/404.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import Header from "../Template/Header"
+import Footer from "../Template/Footer"
+
+const ErrorNotFound = () => {
+    return (
+        <>
+            <Header />
+           <p>404</p>
+            <Footer />
+        </>
+    )
+}
+
+export default ErrorNotFound
