a87f67747a7c7f51e1c8948651d766f7d4efbf12
yelmouss <yassine.elmouss@sfr.fr>
2023-01-18 11:45:21 +0100

    404 content

--- src/components/pages/404.jsx
+++ src/components/pages/404.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 
 import Header from "../Template/Header"
 import Footer from "../Template/Footer"
+import Error404 from "../notFound/content"
+
+
+
 
 const ErrorNotFound = () => {
     return (
         <>
             <Header />
-          
+            <Error404 />
             <Footer />
         </>
     )
